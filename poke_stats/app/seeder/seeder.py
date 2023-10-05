f36c4b6c2cca77132f521f09c3674d1309971bd5
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2023-10-04 22:01:47 -0500

    feat: finish poke_start

--- poke_stats/app/seeder/seeder.py
+++ poke_stats/app/seeder/seeder.py
@@ -4,15 +4,23 @@ from app.config.mongo import pokemons
 import csv
 
 
+
+def is_pokemons_empty():
+    return pokemons.count_documents({}) == 0
+
+
 def run():
-    print("Seeding pokemons")
-    csvfile = open('Pokemon.csv', 'r')
-    reader = csv.DictReader(csvfile)
-    for each in reader:
-        row = {}
-        for field in reader.fieldnames:
-            row[field] = each[field]
-        pokemons.insert_one(row)
+    if is_pokemons_empty():
+        print("Seeding pokemons")
+        csvfile = open('Pokemon.csv', 'r')
+        reader = csv.DictReader(csvfile)
+        for each in reader:
+            row = {}
+            for field in reader.fieldnames:
+                row[field] = each[field]
+            pokemons.insert_one(row)
+    else:
+        print("Pokemons collection already contains data. Skipping seeding.")
     
   
 if __name__ == "__main__":
